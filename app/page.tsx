82696ea77852c068098b2da5c6a9d0c996d9c0a3
Samit <Samit.prsl@gmail.com>
2024-04-24 10:43:38 +0530

    Navbar test

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 
 import Navbar from "@/Components/Navbar";
-import Video from "@/Components/Video";
-import Scroller from "@/Components/Scroller";
-import Banner from "@/Components/Banner";
-import YtLink from "@/Components/YtLink";
-import Banner2 from "@/Components/Banner2";
-import Faq from "@/Components/Faq";
-import Footer from "@/Components/Footer";
-import YtLinkPhone from "@/Components/YtLinkPhone";
+// import Video from "@/Components/Video";
+// import Scroller from "@/Components/Scroller";
+// import Banner from "@/Components/Banner";
+// import YtLink from "@/Components/YtLink";
+// import Banner2 from "@/Components/Banner2";
+// import Faq from "@/Components/Faq";
+// import Footer from "@/Components/Footer";
+// import YtLinkPhone from "@/Components/YtLinkPhone";
 export default function Home() {
   return (
     <>
     <Navbar/>
-    <Video/>
+    {/* <Video/>
     <Scroller/>
     <Banner/>
     <div className=" lg:block hidden">
@@ -23,7 +23,7 @@ export default function Home() {
     </div>
     <Banner2/>
     <Faq/>
-    <Footer/>
+    <Footer/> */}
     </>
   );
 }
